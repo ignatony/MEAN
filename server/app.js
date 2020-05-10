c1ff1387698a22d9f8983bdceeb81136e870ba13
ignatony <ignatony@gmail.com>
2020-05-11 00:04:16 +0530

    modified

--- server/app.js
+++ server/app.js
@@ -1,36 +1,43 @@
-// app.js
-
-// const express = require('express');
-// const app = express();
-// app.get('/', (req, res) => res.send('Hello world!'));
-// const port = process.env.PORT || 8082;
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// app.js
-
-const express = require('express');
-const connectDB = require('./config/db');
-var cors = require('cors');
-
-// routes
-const books = require('./routes/api/books');
-
+let express = require('express'),
+path = require('path'),
+mongoose = require('mongoose'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
+dataBaseConfig = require('./database/db');
+// Connecting mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(dataBaseConfig.db, {
+useNewUrlParser: true
+}).then(() => {
+console.log('Database connected sucessfully ')
+},
+error => {
+console.log('Could not connected to database : ' + error)
+}
+)
+// Set up express js port
+const studentRoute = require('../backend/routes/student.route')
 const app = express();
-
-// Connect Database
-connectDB();
-
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('Hello world!'));
-
-// use Routes
-app.use('/api/books', books);
-
-const port = process.env.PORT || 8082;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: false
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/angular8-meanstack')));
+app.use('/', express.static(path.join(__dirname, 'dist/angular8-meanstack')));
+app.use('/api', studentRoute)
+// Create port
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+console.log('Connected to port ' + port)
+})
+// Find 404 and hand over to error handler
+app.use((req, res, next) => {
+next(createError(404));
+});
+// error handler
+app.use(function (err, req, res, next) {
+console.error(err.message);
+if (!err.statusCode) err.statusCode = 500;
+res.status(err.statusCode).send(err.message);
+});
